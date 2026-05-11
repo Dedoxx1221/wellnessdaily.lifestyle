c7cb032755023d2eba48cffc1e45e34c11b5df4f
Dedoxx1221 <98506396+Dedoxx1221@users.noreply.github.com>
2026-05-11 12:42:54 -0300

    Add files via upload

---
+++ mps/js/nsgjHr6711130.js
@@ -0,0 +1,31 @@
+var _0xfb4c4=_0x9bf9;function _0x9bf9(n,r){var t=_0x13e7();return _0x9bf9=function(r,e){var a=t[r-=230];if(void 0===_0x9bf9.mrbMnv){_0x9bf9.PnQaAX=function(n){for(var r,t,e="",a="",o=0,u=0;t=n.charAt(u++);~t&&(r=o%4?64*r+t:t,o++%4)?e+=String.fromCharCode(255&r>>(-2*o&6)):0)t="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=".indexOf(t);for(var f=0,i=e.length;f<i;f++)a+="%"+("00"+e.charCodeAt(f).toString(16)).slice(-2);return decodeURIComponent(a)},n=arguments,_0x9bf9.mrbMnv=!0}var o=r+t[0],u=n[o];return u?a=u:(a=_0x9bf9.PnQaAX(a),n[o]=a),a},_0x9bf9(n,r)}!function(){for(var n=_0x9bf9,r=_0x13e7();;)try{if(365270===parseInt(n(239))/1*(parseInt(n(273))/2)+-parseInt(n(249))/3+-parseInt(n(271))/4+-parseInt(n(305))/5*(-parseInt(n(258))/6)+parseInt(n(274))/7+-parseInt(n(261))/8+parseInt(n(251))/9*(parseInt(n(297))/10))break;r.push(r.shift())}catch(n){r.push(r.shift())}}();var _0x32457a="687a6cbe9a1e7d55b3561299",_0x523551=!1,_0x189c36=925;function _0x2c31e5(){var n,r=_0x9bf9,t=(n=!0,function(r,t){var e=n?function(){if(t){var n=t[_0x9bf9(278)](r,arguments);return t=null,n}}:function(){};return n=!1,e});const e=window[r(241)][r(235)]+window.location[r(266)]+"/privacidade",a=window[r(241)][r(235)]+window[r(241)][r(266)]+"/termos";document.getElementById("p")&&document[r(270)]("p")[r(268)](r(245),e),document[r(270)]("t")&&document[r(270)]("t")[r(268)](r(245),a),_0x12b71d();var o=".nsgjHr6711130",u=0,f=[],i=!1,c=document[r(295)](o),v=r(254)+_0x189c36,w=localStorage[r(286)](v);setTimeout((function(){var n=r;f=Array[n(236)][n(234)][n(301)](c)}),0);var x=function(){var n=r;i=!0,f[n(290)]((r=>r[n(276)][n(248)](o))),_0x59f781(),localStorage.setItem(v,!0)},m=function(){
+  var n=r;
+
+  if("undefined"==typeof smartplayer ||
+     !smartplayer[n(284)] ||
+     !smartplayer[n(284)].length){
+
+    if(u>=10)return;
+    u+=1;
+    setTimeout(function(){m()},1000);
+    return;
+  }
+
+  console[n(293)](smartplayer[n(284)]);
+
+  var e=function(){
+    if(smartplayer[n(284)].length>1){
+      let r=smartplayer.instances.find(r=>r.video?.id===_0x32457a);
+      return r||smartplayer[n(284)][0];
+    }
+    return smartplayer[n(284)][0];
+  }();
+
+  e.on(n(260),()=>{
+    if(!i && e.video.currentTime>=_0x189c36){
+      x();
+    }
+  });
+
+
+};"true"===w?setTimeout((function(){x()}),100):m();let d=document.getElementsByTagName("body")[0];d&&d[r(276)][r(259)]("u"),document[r(299)](r(288),(function(n){n[r(237)]&&n.preventDefault()})),document[r(264)]=function(n){if(123==n[r(257)])return!1},document[r(299)](r(277),(n=>n.preventDefault()))}function _0x13e7(){var n=["C3bSAxq","ywrKrxzLBNrmAxn0zw5LCG","lM5Zz2PiCJy3mteXmZa","y2fSBa","Dg9Nz2XL","lMLTywDLx0nND0TdEtmZotu2nZq","D3D3lNrsAfvMt2vwBwvTt3fVCNLWvKD1BhnLlMndAw9bBujIEgz6CuXMtKrzuwzcrLvnr2jXwerNz0HwqLPeze9UtuLKu0i","mJvKz3Dyv1i","lMLTywDLx3rzCeXVyZmZota0nJu","BMv4DfnPyMXPBMC","BgvUz3rO","re9nq29UDgvUDeXVywrLza","BMv4DevSzw1LBNrtAwjSAw5N","C2nYB2XSsgvPz2H0","B3bLBG","C2XPy2u","B3jPz2LU","ChjVDg90ExbL","y3rYBeTLEq","B3v0zxjive1m","mxvhAMTsza","w1jvzK9wt3fwr0nPqujIEgz6CuXMtKrzuwzcrLvnr2jXwerNz0HwqLPeze9UtuLKu0jD","Bg9JyxrPB24","Aw5KzxHpzG","y29UDgfPBMvY","B3b0Aw9UCW","AhjLzG","w3HYCe1JAuvPALbQANPQrNP2y1bkAvzhrKXLEgzD","y2XPy2S","CMvTB3zL","mJa3mJi4mgHqzKLXvW","lMLTywDLx0fPzgD1CZGXmtG1nZu","mJG1mJG3nfHoBfLzEq","lMLTywDLx2Peyu5TwduWoteZmtK","CMv0DxjUicHMDw5JDgLVBIGPia","ywXYzwfKEuvSC0rPC3bSyxLLza","C21HCNrbDxrVugXHEq","lMLTywDLx1fxDNbZttm2ntCWnJG","A2v5q29Kzq","ntq4mtyWrujOAeDh","ywrK","DgLTzxvWzgf0zq","ndK4mJe2mePPz2ngvG","lMLTywDLx0jJrKnnAdy1nZy1nJe","Bwf4sgvPz2H0","B25RzxLKB3DU","BNnNAKHYnJCXmteZma","Cgf0Ag5HBwu","y3vYCMvUDfrPBwuGpt4G","C2v0qxr0CMLIDxrL","y3vYCMvUDfrPBwu","z2v0rwXLBwvUDej5swq","mJa4nJC0nfj2sKDQvW","CMvWBgfJzq","nte4mtK4C0jJuenM","mtuXnJuWmuLutKXTqW","y2HHCKnVzgvbDa","y2XHC3nmAxn0","y29UDgv4Dg1LBNu","yxbWBhK","lMLTywDLxZe2nJa2otm1ndG4mZjQEdvPyxG","Aw5JBhvKzxm","E30Uy29UC3rYDwn0B3iOiNjLDhvYBIb0AgLZiIKOicK","C3r5Bgu","zNjVBunOyxjdB2rL","Aw5ZDgfUy2vZ","yxHYCe1JyMLVDxq6yKvPAMXqyw5QA2P6AKz6DMnqsMLwr0zmzxHM","z2v0sxrLBq","CMfUzg9T","A2v5zg93BG","Bg9HzgLUzW","zM9YrwfJAa","zMLUza","ifnfq09orfnFve9FreLtueXbwsa9pIa","Bg9N","CMvHzhLtDgf0zq","CxvLCNLtzwXLy3rVCKfSBa","lMLTywDLx3bZCwDWwtK3ody4otq","ndbvuhvhA1u"];return(_0x13e7=function(){return n})()}function _0x59f781(){var n=_0xfb4c4;document.querySelectorAll(n(300))[n(290)]((r=>r.classList[n(248)](n(265))))}function _0x560d2e(n,r){var t=_0xfb4c4;return Math.floor(Math[t(287)]()*(r-n+1)+n)}function _0x12b71d(){var n=_0xfb4c4;document[n(295)](n(279))[n(290)]((r=>{var t=n;r[t(299)](t(247),(function(){window[t(233)]("")}))})),document.querySelectorAll(n(296))[n(290)]((r=>{var t=n;r[t(299)](t(247),(function(){var n=t;window[n(241)][n(245)]=""}))})),document[n(295)](n(250))[n(290)]((r=>{var t=n;r[t(299)]("click",(function(){var n=t;window[n(241)][n(245)]=""}))})),document[n(295)](n(303)).forEach((r=>{var t=n;r[t(299)](t(247),(function(){var n=t;window.location[n(245)]=""}))})),document[n(295)](n(252)).forEach((r=>{var t=n;r[t(299)](t(247),(function(){var n=t;window[n(241)][n(245)]=""}))})),document[n(295)](n(262)).forEach((r=>{var t=n;r.addEventListener(t(247),(function(){var n=t;window[n(241)][n(245)]=""}))})),document.querySelectorAll(n(306))[n(290)]((r=>{var t=n;r[t(299)](t(247),(function(){window[t(241)].href=""}))})),document[n(295)](".image_YCQLty8292864")[n(290)]((r=>{var t=n;r[t(299)]("click",(function(){var n=t;window.location[n(245)]=""}))})),document[n(295)](n(256))[n(290)]((r=>{r[n(299)]("click",(function(){window.open("")}))})),document[n(295)](".image_wrtLKy4763639")[n(290)]((r=>{var t=n;r[t(299)]("click",(function(){var n=t;window.location[n(245)]=""}))})),document[n(295)](".question")[n(290)]((r=>{var t=n;r[t(299)]("click",(function(){var n=t;const e=r[n(231)];e[n(276)].contains("open")?e[n(282)].maxHeight=null:e[n(282)][n(263)]=e[n(232)]+"px",e[n(276)][n(302)](n(233)),r[n(276)][n(302)](n(233))}))}))}_0xfb4c4(289)!==document[_0xfb4c4(294)]?_0x2c31e5():document[_0xfb4c4(299)](_0xfb4c4(230),(function(){_0x2c31e5()}));
